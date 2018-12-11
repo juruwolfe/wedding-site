d622ada3d9aaf341f4dfd698c15b4ca6c85bfee3
Wolfe <juruwolfe@gmail.com>
2018-12-11 09:13:58 -0500

    terrible confetti

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,31 @@
 // Test for hot-reload
 require('../scss/styles.scss');
+import $ from 'jquery';
 
-document.getElementById('content').textContent = 'BOOYAH!'
 
-// Tests for ES6
-document.addEventListener('click', e => {
-  console.log(e.target);
-})
+class BgEffect {
+	constructor(effect){
+		this.colors = 8;
+		this[effect]();
+	}
+
+	confetti(){
+		this.confetti_size = 2000;
+		let squares = Math.floor((window.innerWidth*window.innerHeight)/this.confetti_size);
+
+		let current_square = 0;
+
+
+		while (current_square < squares) {
+			current_square ++;
+			let new_confett = `<div class="confetti color-${Math.round((Math.random()*this.colors)+1)}"></div>`
+			$('.bg-group')
+				.append(new_confett);
+		}
+	}
+}
+
+let confetti = new BgEffect("confetti");
 
 // Required on main file to accept hot reload
 if (module.hot) {
