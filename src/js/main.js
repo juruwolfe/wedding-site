64a7f859160e0e4ccd3ca6920f2eb2e75ef1c1f8
Wolfe <juruwolfe@gmail.com>
2019-01-05 16:02:44 -0500

    diamonds

--- src/js/main.js
+++ src/js/main.js
@@ -4,13 +4,14 @@ import $ from 'jquery';
 
 
 class BgEffect {
-	constructor(effect){
-		this.colors = 8;
+	constructor(effect, selection){
+		this.selector = selection;
 		this[effect]();
 	}
 
 	confetti(){
-		this.confetti_size = 2000;
+		this.confetti_size = 2700;
+		this.colors = 8;
 		let squares = Math.floor((window.innerWidth*window.innerHeight)/this.confetti_size);
 
 		let current_square = 0;
@@ -19,13 +20,36 @@ class BgEffect {
 		while (current_square < squares) {
 			current_square ++;
 			let new_confett = `<div class="confetti color-${Math.round((Math.random()*this.colors)+1)}"></div>`
-			$('.bg-group')
+			$(this.selector).children('.bg-group')
 				.append(new_confett);
 		}
 	}
+
+	diamonds(){
+		this.confetti_size = 2600;
+		let squares = Math.floor(($(this.selector).width()*$(this.selector).height())/this.confetti_size);
+
+		let current_square = 0;
+		console.log($(this.selector).width())
+
+		while (current_square < squares) {
+			current_square ++;
+			let new_diamond = `<div class="diamond"></div>`
+			$(this.selector).children('.bg-group')
+				.append(new_diamond);
+		}
+
+		/*setInterval(()=>{
+			let seed = (Math.round(Math.random()*9)+1);
+
+			$(".diamond").removeClass('sparkle');
+			$(`.diamond:nth-child(3n+${seed})`).addClass('sparkle');
+		}, 500)*/
+	}
 }
 
-let confetti = new BgEffect("confetti");
+let confetti = new BgEffect("confetti", '#header');
+let diamonds = new BgEffect("diamonds", '#schedule');
 
 
 
