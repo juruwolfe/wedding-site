4d500fee460ee2ed908ef36ce477f311779bb809
Wolfe <juruwolfe@gmail.com>
2019-01-13 12:49:29 -0500

    latest build

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,12 @@
 require('../scss/styles.scss');
 import $ from 'jquery';
 
+const isOnScreen = (element) => {
+    var curPos = element.offset();
+    var curTop = curPos.top;
+    var screenHeight = $(window).height();
+    return (curTop > screenHeight) ? false : true;
+}
 
 class BgEffect {
 	constructor(effect, selection){
@@ -23,10 +29,11 @@ class BgEffect {
 			$(this.selector).children('.bg-group')
 				.append(new_confett);
 		}
+		$(`.confetti:nth-child(3n+5`).addClass('spin');
 	}
 
 	diamonds(){
-		this.confetti_size = 1200;
+		this.confetti_size = 1150;
 		let squares = Math.floor(($(this.selector).width()*$(this.selector).height())/this.confetti_size);
 
 		let current_square = 0;
@@ -39,17 +46,51 @@ class BgEffect {
 				.append(new_diamond);
 		}
 
-		setInterval(()=>{
-			let seed = (Math.round(Math.random()*9)+1);
+		let sparkle;
 
-			$(".diamond").removeClass('sparkle');
-			$(`.diamond:nth-child(3n+${seed})`).addClass('sparkle');
-		}, 500)
+		$(this.selector+" .bg-group").hover(function() {
+			sparkle = window.setInterval(() => {
+						let seed = (Math.round(Math.random()*9)+1);
+
+						$(".diamond").removeClass('sparkle');
+						$(`.diamond:nth-child(3n+${seed})`).addClass('sparkle');
+					}, 500)
+		}, function() {
+			clearInterval(sparkle);
+		});
+	}
+
+	hearts(){
+		this.bg_size = 550;
+		let hearts = Math.floor(($(this.selector).width()*$(this.selector).height())/this.bg_size);
+
+		let current_heart = 0;
+		console.log($(this.selector).width())
+
+		while (current_heart < hearts) {
+			current_heart ++;
+			let new_heart = `<div class="heart"></div>`
+			$(this.selector).children('.bg-group')
+				.append(new_heart);
+		}
+
+		let heart_sparkle;
+		$(this.selector+" .bg-group").hover(function() {
+			heart_sparkle = window.setInterval(() => {
+				let seed = (Math.round(Math.random()*9)+1);
+
+				$(".heart").removeClass('sparkle');
+				$(`.heart:nth-child(9n+${seed})`).addClass('sparkle');
+			}, 1000)
+		}, function() {
+			clearInterval(heart_sparkle);
+		});
 	}
 }
 
 let confetti = new BgEffect("confetti", '#header');
 let diamonds = new BgEffect("diamonds", '#schedule');
+let hearts = new BgEffect("hearts", '#rsvp');
 
 
 
@@ -63,6 +104,8 @@ const linkFillEffect = () => {
 }
 
 linkFillEffect();
+
+
 
 // Required on main file to accept hot reload
 if (module.hot) {
